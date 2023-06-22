4fd4e695d19434033adac0d33d4b0d41f8e5c2ce
Jess Ayers <jessayers@Jesss-MBP.attlocal.net>
2023-06-22 11:52:48 -0400

    README setup.

---
+++ README.R
@@ -0,0 +1,4 @@
+rmarkdown::render("/Users/jessayers/Documents/ST 558/TOPIC 2/Project-1/README.Rmd",
+                  output_format = "github_document",  
+                  output_dir = "/Users/jessayers/Documents/ST 558/TOPIC 2/Project-1" ,
+                  output_options = list(html_preview = FALSE))
